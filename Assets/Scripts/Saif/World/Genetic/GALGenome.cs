29c2d12521faf209345519d93280deb343419eda
jurilents <keeeneeek@gmail.com>
2022-10-20 22:44:52 +0300

    Genetic core

---
+++ Assets/Scripts/Saif/World/Genetic/GALGenome.cs
@@ -0,0 +1,20 @@
+using Saif.World.ArtificialLife;
+
+namespace Saif.World.Genetic
+{
+    public class GALGenome : IGenome
+    {
+        private const int Capacity = 8;
+
+        public void Next() { }
+
+
+        private static class Genes
+        {
+            public static void MoveForward(BioUnit unit)
+            {
+                
+            }
+        }
+    }
+}
